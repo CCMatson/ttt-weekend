c18ad85208810b1722aa2c867a6d8fd89081293a
CCMatson <catherine.matson@gmail.com>
2022-12-09 17:37:11 -0800

    initializing the game board

--- js/app.js
+++ js/app.js
@@ -1,18 +1,135 @@
 /*-------------------------------- Constants --------------------------------*/
 
 
+// const sqIdx = squareEls.slice()
+
+
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 3, 6],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 
 /*---------------------------- Variables (state) ----------------------------*/
+let board, turn, winner, tie
 
+
+// board = [null, null, null, null, null, null, null, null, null]
+
+// turn = 1
+// winner = false
+// tie = false
 
 
 /*------------------------ Cached Element References ------------------------*/
-
-
+const squareEls = document.getElementsByClassName("sqr")
+console.log(squareEls)
+const messageEl = document.getElementById("message")
+console.log(messageEl)
+const boardEl = document.querySelector(".board")
+console.log(boardEl)
 
 /*----------------------------- Event Listeners -----------------------------*/
+boardEl.addEventListener('click', handleClick) 
+  console.log()
+//console.log(evt)
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+//stores the game state
+function init(){
+  board = [1, null, -1, null, null, -1, null, null, null]
+  turn = 1 
+  winner = false
+  tie = false
+render()
+}
 
+//calls init
+init()
+
+
+function render(){
+  // updateBoard()
+  // updateMessage()
+console.log('sanity check2')
+}
+
+// displays game state of each x , o , or null 
+function upBoard(){
+  console.log(board)
+board.forEach(function(element, index){
+  if (element === 1){
+    console.log(element)
+    squareEls[index].textContent = "X"
+    console.log(squareEls[index])
+  } if (element === -1){
+    console.log(index)
+    squareEls[index].textContent = "O"
+    console.log(squareEls)
+    console.log(squareEls[index])
+  }
+} ) 
+}
+
+upBoard()
+
+function updateMessage(){
+  if (winner === false && tie === false){
+  console.log `It's ${turn}'s turn`
+  } if (winner === false && tie === true){
+    `It's a tie`
+  }if (winner === true && tie === false){
+  console.log `${turn} Wins`
+  }  
+}
+updateMessage()
+
+function handleClick(evt){
+  const sqIdx = evt.target.id.at(-1)
+  console.log(sqIdx)
+
+  if (board[sqIdx] === 1 || board[sqIdx] === -1){
+    return
+  }
+  if (winner === true){
+    return
+  }
+  
+  console.log(sqIdx)
+  console.log('handle click works')
+  console.log(evt)
+}
+
+function placePiece(idx){
+  board[idx] = turn
+  console.log(board[idx])
+  console.log(turn)
+  }
+
+placePiece()
+
+function checkForTie(){
+  if (!board.includes("null")) tie = true
+}
+console.log(tie)
+checkForTie()
+
+//totals the numbers in each arrays array
+// if it's 3 , winner
+// layer problem, attach this to the board
+//math abs is the condition inside if statement
+function checkForWinner(){
+for (let i = 0; i < winningCombos.length; i++){
+  Math.abs(winningCombos[i][0] + winningCombos[i][1] + winningCombos[i][2])
+  } 
+//console.log(x)
+}
+checkForWinner()
